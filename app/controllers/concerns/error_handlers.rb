abb0f9cb6d23e45c9768005d02186de32672e7d9
Kent Fujii <kent.where.the.light.is@gmail.com>
2015-09-08 00:09:33 +0900

    ajax_post

--- app/controllers/concerns/error_handlers.rb
+++ app/controllers/concerns/error_handlers.rb
@@ -3,9 +3,8 @@ module ErrorHandlers
 
   included do
     rescue_from Exception, with: :rescue500
+    rescue_from ActionController::BadRequest, with: :rescue400
     rescue_from ActionController::ParameterMissing, with: :rescue400
-    rescue_from ApplicationController::Forbidden, with: :rescue403
-    rescue_from ApplicationController::IpAddressRejected, with: :rescue403
     rescue_from ActionController::RoutingError, with: :rescue404
     rescue_from ActiveRecord::RecordNotFound, with: :rescue404
   end
