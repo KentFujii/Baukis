3e3df4ff69ed3a4f702ac0379f5a8135e473816c
Kent Fujii <kent.where.the.light.is@gmail.com>
2015-09-14 10:02:40 +0900

    hash_lock

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150911135652) do
+ActiveRecord::Schema.define(version: 20150914003833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -102,6 +102,16 @@ ActiveRecord::Schema.define(version: 20150911135652) do
 
   add_index "entries", ["customer_id"], name: "index_entries_on_customer_id", using: :btree
   add_index "entries", ["program_id", "customer_id"], name: "index_entries_on_program_id_and_customer_id", unique: true, using: :btree
+
+  create_table "hash_locks", force: :cascade do |t|
+    t.string   "table",      null: false
+    t.string   "column",     null: false
+    t.string   "key",        null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "hash_locks", ["table", "column", "key"], name: "index_hash_locks_on_table_and_column_and_key", unique: true, using: :btree
 
   create_table "message_tag_links", force: :cascade do |t|
     t.integer  "message_id", null: false
